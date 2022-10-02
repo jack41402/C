41e232bdd5b6c59aa6ce19ccb4cad2e3e3bfe7c7
jack41402 <lucky368368@gmail.com>
2022-10-02 21:27:25 +0800

    initialize

---
+++ FCU/assgn1_D1166506/cmake-build-debug/CMakeFiles/assgn1_D1166506.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assgn1_D1166506.dir/main.c.obj"
+  "assgn1_D1166506.exe"
+  "assgn1_D1166506.exe.manifest"
+  "assgn1_D1166506.pdb"
+  "libassgn1_D1166506.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assgn1_D1166506.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
