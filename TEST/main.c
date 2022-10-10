15b70e051b4fd3cf43348cec6dc6090c24734a24
jack41402 <lucky368368@gmail.com>
2022-10-10 17:43:03 +0800

    update

--- TEST/main.c
+++ TEST/main.c
@@ -8,6 +8,8 @@ int main()
         printf ("Enter: ") , fflush(stdout) ;
         fflush(stdin) ;
         flag = scanf ("%d %d %d" , &a , &b , &c) ;
+        printf ("%d %d %d\n" , a , b , c) ;
+        printf ("%d" , flag) ;
     }
     while (flag != 3) ;
     return 0;
