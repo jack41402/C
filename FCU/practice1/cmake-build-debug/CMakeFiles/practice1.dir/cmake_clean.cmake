cee8228124a3732c07b43472acfda7e9a340cc10
jack41402 <lucky368368@gmail.com>
2022-10-17 21:20:33 +0800

    update

---
+++ FCU/practice1/cmake-build-debug/CMakeFiles/practice1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practice1.dir/main.c.obj"
+  "CMakeFiles/practice1.dir/main.c.obj.d"
+  "libpractice1.dll.a"
+  "practice1.exe"
+  "practice1.exe.manifest"
+  "practice1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/practice1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
