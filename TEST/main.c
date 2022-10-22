cc8f73737d7d31907e149a2688340dd296241fe5
jack41402 <lucky368368@gmail.com>
2022-10-23 01:58:44 +0800

    update

--- TEST/main.c
+++ TEST/main.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 
-//Remember to use flag V to prevent from overflow
-
-int adder (int a , int b , int carry)
+void printBinary(unsigned int n) // Print the integer variable in binary.
 {
-    int ans=0 ;
-    for (int i=0 ; i<=32 ; ++i)
+    for (int i=1 ; i<=32 ; i++) // Output all 32 bits of the variable in binary.
     {
-        int A_bit=(a>>i)&1 , B_bit=(b>>i)&1 ;
-        //Get the bit of integer variables a and b ;
-        ans |= ((((a>>i) & 1 ^ (b>>i) & 1) ^ carry)<<i) ;
-        carry = ((((a>>i)&1) & ((b>>i)&1)) | (((a>>i) & 1 ^ (b>>i) & 1) & carry)) ;
-        //the version below won't generate any warning.
-        //carry = ((A_bit & B_bit) | (((a>>i) & 1 ^ (b>>i) & 1) & carry)) ;
+        printf ("%d" , (n>>(32-i))&1) ; // Output bits of n from left to right.
+        if (i%4==0) printf (" ") ; // Print space every four bits.
     }
-    return ans ;
+}
+
+void adder (int X , int Y , int *carry , int *ans , int *overflow) // full adder
+{
+    for (int i=0 ; i<32 ; ++i)
+    {
+        *ans |= ((((X>>i) & 1 ^ (Y>>i) & 1) ^ *carry)<<i) ; // Use Leftshift and OR to add the C_in^(X^Y) to the i-th digit.
+        *overflow = *carry , *carry = ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry)) , *overflow ^= *carry ; // To determine whether to carry in a normal situation, one calculates ((X & Y)|(XY) & carry).
+    }
 }
 
 int main()
 {
-    int a , b , m=0 ;
-    char sign ;
-    scanf ("%d%c%d" , &a , &sign , &b) ;
-    if (sign=='-') m = 1 , b = ~b ;
-    printf ("%d\n" , adder (a , b , m)) ;
+    while (1)
+    {
+        int X , Y , carry=0 , ans=0 , overflow ;
+        char operator ;
+        scanf("%d %c %d", &X, &operator, &Y) ;
+        if (X==0 && Y==0) break ; // Break the loop while X and Y are both equal to 0.
+        if (operator == '-') carry = 1 ; // Carry in at the first digit while the operator is negative.
+        adder(X , (operator=='-') ? ~Y : Y , &carry , &ans , &overflow) ; // Use the function adder to sum up X and Y.
+        if (overflow) printf("Overflow!\n") , fflush(stdout) ;  // Use carry to catch the flag
+        else // print the variables X, Y, and S in both decimal and binary.
+        {
+            printf ("%d\n\n" , overflow) ;
+            printf ("X = %-10d  Binary value: " , X) , printBinary (X) , printf ("\n") ;
+            printf ("Y = %-10d  Binary value: " , Y) , printBinary (Y) , printf ("\n") ;
+            printf ("S = %-10d  Binary value: " , ans) , printBinary (ans) , printf ("\n") ;
+            fflush(stdout) ;
+        }
+    }
     return 0;
 }
