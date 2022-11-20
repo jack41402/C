bebf1fe7fd722b7d0a7e27258d5b85813b3c122f
jack41402 <lucky368368@gmail.com>
2022-11-21 05:02:36 +0800

    Update main.c

--- TEST/main.c
+++ TEST/main.c
@@ -1,42 +1,101 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
 
-void printBinary(unsigned int n) // Print the integer variable in binary.
-{
-    for (int i=1 ; i<=32 ; i++) // Output all 32 bits of the variable in binary.
-    {
-        printf ("%d" , (n>>(32-i))&1) ; // Output bits of n from left to right.
-        if (i%4==0) printf (" ") ; // Print space every four bits.
+int max(int x, int y) {
+    if (x>y) return x;
+    else return y;
+}
+
+int min(int x, int y) {
+    if (x<y) return x;
+    else return y;
+}
+
+void LU_decompose(int n, float A[100][100], float L[100][100], float U[100][100]) {
+    int i, j, k;
+
+    for (k=0; k<n; k++) { // To compute submatrix A(k+1).
+        for (j=k; j<n; j++) U[k][j] = A[k][j]; // Compute elements of the k-th row of matrix U
+        for (i=k; i<n; i++) L[i][k] = A[i][k] / A[k][k]; // Compute elements of the k-th column of matrix L
+        for (i=k+1; i<n; i++) // Compute elements of submatrix A(k+1):
+            for (j=k+1; j<n; j++) A[i][j] = A[i][j] - L[i][k] * U[k][j];
     }
 }
 
-void adder (int X , int Y , int *carry , int *ans , int *overflow) // full adder
-{
-    for (int i=0 ; i<32 ; ++i)
-    {
-        *ans |= ((((X>>i) & 1 ^ (Y>>i) & 1) ^ *carry)<<i) ; // Use Leftshift and OR to add the C_in^(X^Y) to the i-th digit.
-        *overflow = *carry , *carry = ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry)) , *overflow ^= *carry ; // To determine whether to carry in a normal situation, one calculates ((X & Y)|(XY) & carry).
-    }
-}
 
-int main()
-{
-    while (1)
-    {
-        int X , Y , carry=0 , ans=0 , overflow ;
-        char operator ;
-        scanf("%d %c %d", &X, &operator, &Y) ;
-        if (X==0 && Y==0) break ; // Break the loop while X and Y are both equal to 0.
-        if (operator == '-') carry = 1 ; // Carry in at the first digit while the operator is negative.
-        adder(X , (operator=='-') ? ~Y : Y , &carry , &ans , &overflow) ; // Use the function adder to sum up X and Y.
-        if (overflow) printf("Overflow!\n") , fflush(stdout) ;  // Use carry to catch the flag
-        else // print the variables X, Y, and S in both decimal and binary.
-        {
-            printf ("%d\n\n" , overflow) ;
-            printf ("X = %-10d  Binary value: " , X) , printBinary (X) , printf ("\n") ;
-            printf ("Y = %-10d  Binary value: " , Y) , printBinary (Y) , printf ("\n") ;
-            printf ("S = %-10d  Binary value: " , ans) , printBinary (ans) , printf ("\n") ;
-            fflush(stdout) ;
+int main(void) {
+    float A[100][100], A1[100][100], L[100][100], U[100][100]; // Assume maximum 100 by 100 elements.
+    int n; // Size of the square matrices.
+    int r, s; // Lower bandwidth and upper bandwidth of matrix A.
+
+    int check; // Flag to check the correctness of the program.
+    int i, j, k; // Loop variables.
+
+    srand(0); // Seed of the random number generator.
+
+    printf("Enter square matrix size n: "); // Input the matrix size.
+    scanf("%d", &n);
+    printf("\nEnter the lower bandwidth and upper bandwidth of matrix A, (r , s): ");
+    scanf("%d %d", &r, &s);
+
+
+    // Get values for the elements of matrix A.
+    // Matrix element A[i][j], 0<A[i][j]<=1; A1[i][j] is the copy of A[i][j] for program verification.
+    for (i=0; i<n; i++)
+        for (j=max(0,i-r); j<=min(n-1,i+s); j++) {A[i][j] = (rand() % 10000 + 1) / 10000.0; A1[i][j] = A[i][j];}
+
+    printf("\nMatrix A:\n"); // Output matrix A.
+    for (i=0; i<n; i++) {
+        for (j=0; j<max(0,i-r); j++) printf("         "); // Print blanks for zero elements on the left-hand-side.
+        for (j=max(0,i-r); j<=min(n-1,i+s); j++) printf("%8.4f ", A[i][j]); // Print non-zero elements.
+        printf("\n");
+    }
+/*
+  for (i=0; i<n; i++) {
+    for (j=0; j<n; j++) printf("%8.4f ", A[i][j]);
+    printf("\n");
+  }
+  */
+
+    LU_decompose(n, A, L, U); // Peform LU-decomposition.
+
+    printf("\nMatrix L:\n"); // Output matrix L.
+    for (i=0; i<n; i++) {
+        for (j=0; j<=i; j++) {
+            if(j<max(0,i-r)){
+                printf("         ");
+            }else{
+                printf("%8.4f ", L[i][j]);
+            }
         }
+        printf("\n");
     }
+
+    printf("\nMatrix U:\n"); // Ouput matrix U.
+    for (i=0; i<n; i++) {
+        for (j=0; j<i; j++) printf("         "); // Do not print the lower triangular elements.
+        for (j=i; j<n; j++) {
+            if(j<=min(n-1,i+s)){
+                printf("%8.4f ", U[i][j]);
+            }else{
+                printf("         ");
+            }
+        }
+        printf("\n");
+    }
+
+    check = 1; // Intial flag to true.
+    for (i=0; i<n && check; i++)
+        for (j=0; j<n && check; j++) {
+            A[i][j] = 0;
+            for (k=0; k<=min(i,j); k++) A[i][j] += L[i][k] * U[k][j]; // Compute A[i][j] for A<=LU
+            check = check && (fabs(A[i][j]-A1[i][j])<0.0001); // Compare A[i][j] and A1[i][j], allow error 0.0001.
+        }
+
+    if (check) printf("\nThe LU-decomposition program is correct.\n"); // The program is correct.
+    else printf("\nThe LU-decomposition program is incorrect.\n"); // The program is incorrect.
+
     return 0;
 }
