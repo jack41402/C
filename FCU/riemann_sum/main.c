a50c55df9933ff7a29625e5c68636881e1bb1a75
jack41402 <lucky368368@gmail.com>
2022-11-24 22:59:25 +0800

    practice update

---
+++ FCU/riemann_sum/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n ;
+    printf ("5") ;
+    return 0;
+}
--- TEST/main.c
+++ TEST/main.c
@@ -1,42 +1,20 @@
 #include <stdio.h>
 
-void printBinary(unsigned int n) // Print the integer variable in binary.
-{
-    for (int i=1 ; i<=32 ; i++) // Output all 32 bits of the variable in binary.
-    {
-        printf ("%d" , (n>>(32-i))&1) ; // Output bits of n from left to right.
-        if (i%4==0) printf (" ") ; // Print space every four bits.
-    }
-}
+unsigned long long arr[100]={0} ;
 
-void adder (int X , int Y , int *carry , int *ans , int *overflow) // full adder
+unsigned long long Fib (int n)
 {
-    for (int i=0 ; i<32 ; ++i)
+    if (arr[n]==0)
     {
-        *ans |= ((((X>>i) & 1 ^ (Y>>i) & 1) ^ *carry)<<i) ; // Use Leftshift and OR to add the C_in^(X^Y) to the i-th digit.
-        *overflow = *carry , *carry = ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry)) , *overflow ^= *carry ; // To determine whether to carry in a normal situation, one calculates ((X & Y)|(XY) & carry).
+        arr[n] = Fib(n-1) + Fib(n-2) ;
     }
+    else return arr[n];
 }
 
 int main()
 {
-    while (1)
-    {
-        int X , Y , carry=0 , ans=0 , overflow ;
-        char operator ;
-        scanf("%d %c %d", &X, &operator, &Y) ;
-        if (X==0 && Y==0) break ; // Break the loop while X and Y are both equal to 0.
-        if (operator == '-') carry = 1 ; // Carry in at the first digit while the operator is negative.
-        adder(X , (operator=='-') ? ~Y : Y , &carry , &ans , &overflow) ; // Use the function adder to sum up X and Y.
-        if (overflow) printf("Overflow!\n") , fflush(stdout) ;  // Use carry to catch the flag
-        else // print the variables X, Y, and S in both decimal and binary.
-        {
-            printf ("%d\n\n" , overflow) ;
-            printf ("X = %-10d  Binary value: " , X) , printBinary (X) , printf ("\n") ;
-            printf ("Y = %-10d  Binary value: " , Y) , printBinary (Y) , printf ("\n") ;
-            printf ("S = %-10d  Binary value: " , ans) , printBinary (ans) , printf ("\n") ;
-            fflush(stdout) ;
-        }
-    }
-    return 0;
+    arr[0]=1 , arr[1]=1 ;
+    int n ;
+    scanf ("%d" , &n) ;
+    printf ("%llu\n" , Fib(n)) ;
 }
