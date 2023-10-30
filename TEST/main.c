6fa105444bcf55e9d6c392dbd59473cdb21f90e4
Jack <lucky368368@gmail.com>
2023-10-30 13:15:24 +0800

    Update main.c

--- TEST/main.c
+++ TEST/main.c
@@ -1,72 +1,25 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdio.h>
 
-void binary (int n){
-    int i;
-    for(i=0;i<32;i++){
-        printf("%d", (n>>31-i)&1);
-        if(i%4==3) printf(" ");
-    }
-}
+int main(void) {
+	// Name of 12 months.
+	const char *name_of_month[12] = {"January", "February", "March", "April", "May", "June",
+	                        "July", "August", "September", "October", "November", "December"};
+	// Name of seven weekdays.
+	const char *days_of_week[7] = {"Sunday", "Monday", "Tuesday", "Wednesday",
+	                             "Thursday", "Friday", "Saturday"};
+	// Number of days of 12 months, non-leap year.
+	const int days_of_month[12]= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-void fulladder(int x, int y, int m, int *c, int*s){
-    *s = (x^(y^m))^*c;
-    *c = (x&(y^m))|(*c&(x^(y^m)));
+	int ww=1, dd=6, mm=9; // September 6, Labor day of the given year.
+	int month, day, weekday; // A day to be computed.
+	int count; // Number of days from the Labor day to the specified day.
+	int i; // Loop variable
 
-}
-int adder_subtractor (int X, int Y, int *c, int m, int *c2){
-    int S = 0;
-    int i,x,y,s;
-    for(i=0;i<32;i++){
-        x = (X>>i)&1;
-        y = (Y>>i)&1;
-        *c2 = *c;
-        fulladder(x, y, m, c, &s);
-        S = S|(s<<i);
-    }
-    return S;
-}
+	printf("Labor day of a given leap year: %s, %s %d\n\n", days_of_week[ww], name_of_month[mm-1], dd);
 
-int main (){
-    setbuf(stdout, NULL);
-    int X,Y,S,c,m,c2;
-    char op;
-    c = 0;
-    printf("D:\\binary_adder_subtractor\n");
-    while(1){
-        printf("Enter \"X + Y\" or \"X - Y\" (X, Y: -2,147,483,648 to 2,147,483,647): ");
-        scanf("%d %c %d",&X,&op,&Y);
-        if(X == 0 && Y == 0){
-            break;
-        }
-        if(op == '+'){
-            m = 0;
-        }
-        else{
-            m = 1;
-        }
-        c = m;
-        S = adder_subtractor(X, Y, &c, m, &c2);
-        printf("X = %-11d  Binary value: ",X);
-        binary(X);
-        printf("\nY = %-11d  Binary value: ",Y);
-        binary(Y);
-        printf("\nS = %-11d  Binary value: ",S);
-        binary(S);
-        if(m==0){
-            if(X+Y == S) printf("\nCorrect! ");
-            else printf("\nIncorrect! ");
-            printf("Adder-subtractor operation test: %d + %d = %d",X,Y,X+Y);
-        }
-        else {
-            if(X-Y == S) printf("\nCorrect! ");
-            else printf("\nIncorrect! ");
-            printf("Adder-subtractor operation test: %d - %d = %d",X,Y,X-Y);
-        }
-        if(c^c2 == 1) printf("\n**** The addition-subtraction operation is overflow.");
+	// ***** Complete program of the following steps:
+	// Compute and print Valentine's day, February 14.
 
-        printf("\n-----------------------------------------------------\n");
-
-    }
+	// Compute and print Thanksgiving day, the forth Thursday in November.
     return 0;
 }
